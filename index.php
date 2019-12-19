c4e747b30a0ac5eda81033da58a4033c1c7a1317
Simon <meet@simon-frey.eu>
2019-12-19 10:36:17 +0100

    init

---
+++ index.php
@@ -0,0 +1,224 @@
+<html>
+
+<head>
+    <title>lazy-simon.js - Minimal effort 350 byte JavaScript library to lazy load all &lt;img&gt; on your website</title>
+
+ <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+ 
+    <link rel="shortcut icon" type="image/png" href="img/logo.png" />
+
+  <meta name="description" content="Lazy loading the images on your websites is a good idea in 99% of the cases. As all the other lazy load libraries where way to heavy for use case I build lazy-simon.js.As it is only 350 byte in total size, I now can include it on every website I develop without having to worry about performance impacts. Only upsides here 🤩" />
+  <meta name="robots" content="index, follow" />
+  <meta name="referrer" content="always" />
+
+  <!-- Social & Open Graph -->
+  <meta property="og:title" content="lazy-simon.js - Minimal effort 350 byte JavaScript library to lazy load all &lt;img&gt; on your website" />
+  <meta property="og:description" content="Lazy loading the images on your websites is a good idea in 99% of the cases. As all the other lazy load libraries where way to heavy for use case I build lazy-simon.js.As it is only 350 byte in total size, I now can include it on every website I develop without having to worry about performance impacts. Only upsides here 🤩" />
+  <meta property="og:image" content="https://lazy.simon-frey.eu/img/social.jpg"> <!-- include your hosted image full URL -->
+  <meta property="og:url" content="https://lazy.simon-frey.eu/" />
+  <meta name="twitter:title" content="lazy-simon.js - Minimal effort 350 byte JavaScript library to lazy load all &lt;img&gt; on your website">
+  <meta name="twitter:description" content="Lazy loading the images on your websites is a good idea in 99% of the cases. As all the other lazy load libraries where way to heavy for use case I build lazy-simon.js.As it is only 350 byte in total size, I now can include it on every website I develop without having to worry about performance impacts. Only upsides here 🤩" />
+  <meta name="twitter:image" content="https://lazy.simon-frey.eu/img/social.jpg" />
+  <!-- include your hosted image full URL -->
+  <meta name="twitter:card" content="summary_large_image" />
+
+
+  <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <h1>lazy-simon.js</h1>
+    <p>Minimal effort 350 byte JavaScript library to lazy load all &lt;img&gt; on your website</p>
+   <p>Lazy loading the images on your websites is a good idea in 99% of the cases. As all the other lazy load libraries where way to heavy for use case I build lazy-simon.js.</p>
+   <p>As it is <b>only 350 byte in total size</b>, I now can include it on every website I develop without having to worry about performance impacts. Only upsides here 🤩</p>
+ 
+ <h2>Usage</h2>
+   <p>Just include the lazy-simon.js  script at the bottom of your body.</p>
+    <pre><span style='color:#d2cd86; '>&lt;</span>script src<span style='color:#d2cd86; '>=</span><span style='color:#02d045; '>"</span><span style='color:#00c4c4; '>https://lazy.simon-frey.eu/js/lazy_simon.min.js</span><span style='color:#02d045; '>"</span><span style='color:#d2cd86; '>></span><span style='color:#d2cd86; '>&lt;</span><span style='color:#d2cd86; '>/</span>script<span style='color:#d2cd86; '>></span></pre>
+    <h2>Technical info</h2>
+    <p>The script uses a <i>Intersection Observer</i> for the getting the elements that are currently viewed. To check what browser all support this feauture checkout: <a href="https://caniuse.com/#search=intersectionobserver">https://caniuse.com/#search=intersectionobserver</a></p>
+    <p>You can find the unminified version at <a href="https://lazy.simon-frey.eu/js/lazy_simon.min.js">https://lazy.simon-frey.eu/js/lazy_simon.min.js</a> and the script is licensed <a href="mit.txt">MIT</a></p>
+
+<h2>Test area</h2>
+<p>Scroll down to see lazy-simon.js in action.</p>
+
+<div id="testArea">
+<img src="https://source.unsplash.com/random/200x200?sig=1">
+<img src="https://source.unsplash.com/random/200x200?sig=2">
+<img src="https://source.unsplash.com/random/200x200?sig=3">
+<img src="https://source.unsplash.com/random/200x200?sig=4">
+<img src="https://source.unsplash.com/random/200x200?sig=5">
+<img src="https://source.unsplash.com/random/200x200?sig=6">
+<img src="https://source.unsplash.com/random/200x200?sig=7">
+<img src="https://source.unsplash.com/random/200x200?sig=8">
+<img src="https://source.unsplash.com/random/200x200?sig=9">
+<img src="https://source.unsplash.com/random/200x200?sig=10">
+<img src="https://source.unsplash.com/random/200x200?sig=11">
+<img src="https://source.unsplash.com/random/200x200?sig=12">
+<img src="https://source.unsplash.com/random/200x200?sig=13">
+<img src="https://source.unsplash.com/random/200x200?sig=14">
+<img src="https://source.unsplash.com/random/200x200?sig=15">
+<img src="https://source.unsplash.com/random/200x200?sig=16">
+<img src="https://source.unsplash.com/random/200x200?sig=17">
+<img src="https://source.unsplash.com/random/200x200?sig=18">
+<img src="https://source.unsplash.com/random/200x200?sig=19">
+<img src="https://source.unsplash.com/random/200x200?sig=20">
+<img src="https://source.unsplash.com/random/200x200?sig=21">
+<img src="https://source.unsplash.com/random/200x200?sig=22">
+<img src="https://source.unsplash.com/random/200x200?sig=23">
+<img src="https://source.unsplash.com/random/200x200?sig=24">
+<img src="https://source.unsplash.com/random/200x200?sig=25">
+<img src="https://source.unsplash.com/random/200x200?sig=26">
+<img src="https://source.unsplash.com/random/200x200?sig=27">
+<img src="https://source.unsplash.com/random/200x200?sig=28">
+<img src="https://source.unsplash.com/random/200x200?sig=29">
+<img src="https://source.unsplash.com/random/200x200?sig=210">
+<img src="https://source.unsplash.com/random/200x200?sig=211">
+<img src="https://source.unsplash.com/random/200x200?sig=212">
+<img src="https://source.unsplash.com/random/200x200?sig=213">
+<img src="https://source.unsplash.com/random/200x200?sig=214">
+<img src="https://source.unsplash.com/random/200x200?sig=215">
+<img src="https://source.unsplash.com/random/200x200?sig=216">
+<img src="https://source.unsplash.com/random/200x200?sig=217">
+<img src="https://source.unsplash.com/random/200x200?sig=218">
+<img src="https://source.unsplash.com/random/200x200?sig=219">
+<img src="https://source.unsplash.com/random/200x200?sig=220">
+<img src="https://source.unsplash.com/random/200x200?sig=31">
+<img src="https://source.unsplash.com/random/200x200?sig=32">
+<img src="https://source.unsplash.com/random/200x200?sig=33">
+<img src="https://source.unsplash.com/random/200x200?sig=34">
+<img src="https://source.unsplash.com/random/200x200?sig=35">
+<img src="https://source.unsplash.com/random/200x200?sig=36">
+<img src="https://source.unsplash.com/random/200x200?sig=37">
+<img src="https://source.unsplash.com/random/200x200?sig=38">
+<img src="https://source.unsplash.com/random/200x200?sig=39">
+<img src="https://source.unsplash.com/random/200x200?sig=310">
+<img src="https://source.unsplash.com/random/200x200?sig=311">
+<img src="https://source.unsplash.com/random/200x200?sig=312">
+<img src="https://source.unsplash.com/random/200x200?sig=313">
+<img src="https://source.unsplash.com/random/200x200?sig=314">
+<img src="https://source.unsplash.com/random/200x200?sig=315">
+<img src="https://source.unsplash.com/random/200x200?sig=316">
+<img src="https://source.unsplash.com/random/200x200?sig=317">
+<img src="https://source.unsplash.com/random/200x200?sig=318">
+<img src="https://source.unsplash.com/random/200x200?sig=319">
+<img src="https://source.unsplash.com/random/200x200?sig=320">
+<img src="https://source.unsplash.com/random/200x200?sig=321">
+<img src="https://source.unsplash.com/random/200x200?sig=322">
+<img src="https://source.unsplash.com/random/200x200?sig=323">
+<img src="https://source.unsplash.com/random/200x200?sig=324">
+<img src="https://source.unsplash.com/random/200x200?sig=325">
+<img src="https://source.unsplash.com/random/200x200?sig=326">
+<img src="https://source.unsplash.com/random/200x200?sig=327">
+<img src="https://source.unsplash.com/random/200x200?sig=328">
+<img src="https://source.unsplash.com/random/200x200?sig=329">
+<img src="https://source.unsplash.com/random/200x200?sig=3210">
+<img src="https://source.unsplash.com/random/200x200?sig=3211">
+<img src="https://source.unsplash.com/random/200x200?sig=3312">
+<img src="https://source.unsplash.com/random/200x200?sig=3213">
+<img src="https://source.unsplash.com/random/200x200?sig=3214">
+<img src="https://source.unsplash.com/random/200x200?sig=3215">
+<img src="https://source.unsplash.com/random/200x200?sig=3216">
+<img src="https://source.unsplash.com/random/200x200?sig=3217">
+<img src="https://source.unsplash.com/random/200x200?sig=3218">
+<img src="https://source.unsplash.com/random/200x200?sig=3219">
+<img src="https://source.unsplash.com/random/200x200?sig=3220">
+<img src="https://source.unsplash.com/random/200x200?sig=51">
+<img src="https://source.unsplash.com/random/200x200?sig=52">
+<img src="https://source.unsplash.com/random/200x200?sig=53">
+<img src="https://source.unsplash.com/random/200x200?sig=54">
+<img src="https://source.unsplash.com/random/200x200?sig=55">
+<img src="https://source.unsplash.com/random/200x200?sig=56">
+<img src="https://source.unsplash.com/random/200x200?sig=57">
+<img src="https://source.unsplash.com/random/200x200?sig=58">
+<img src="https://source.unsplash.com/random/200x200?sig=59">
+<img src="https://source.unsplash.com/random/200x200?sig=510">
+<img src="https://source.unsplash.com/random/200x200?sig=511">
+<img src="https://source.unsplash.com/random/200x200?sig=512">
+<img src="https://source.unsplash.com/random/200x200?sig=513">
+<img src="https://source.unsplash.com/random/200x200?sig=514">
+<img src="https://source.unsplash.com/random/200x200?sig=515">
+<img src="https://source.unsplash.com/random/200x200?sig=516">
+<img src="https://source.unsplash.com/random/200x200?sig=517">
+<img src="https://source.unsplash.com/random/200x200?sig=518">
+<img src="https://source.unsplash.com/random/200x200?sig=519">
+<img src="https://source.unsplash.com/random/200x200?sig=520">
+<img src="https://source.unsplash.com/random/200x200?sig=521">
+<img src="https://source.unsplash.com/random/200x200?sig=522">
+<img src="https://source.unsplash.com/random/200x200?sig=523">
+<img src="https://source.unsplash.com/random/200x200?sig=524">
+<img src="https://source.unsplash.com/random/200x200?sig=525">
+<img src="https://source.unsplash.com/random/200x200?sig=526">
+<img src="https://source.unsplash.com/random/200x200?sig=527">
+<img src="https://source.unsplash.com/random/200x200?sig=528">
+<img src="https://source.unsplash.com/random/200x200?sig=529">
+<img src="https://source.unsplash.com/random/200x200?sig=5210">
+<img src="https://source.unsplash.com/random/200x200?sig=52511">
+<img src="https://source.unsplash.com/random/200x200?sig=5212">
+<img src="https://source.unsplash.com/random/200x200?sig=5213">
+<img src="https://source.unsplash.com/random/200x200?sig=5214">
+<img src="https://source.unsplash.com/random/200x200?sig=5215">
+<img src="https://source.unsplash.com/random/200x200?sig=5216">
+<img src="https://source.unsplash.com/random/200x200?sig=5217">
+<img src="https://source.unsplash.com/random/200x200?sig=5218">
+<img src="https://source.unsplash.com/random/200x200?sig=5219">
+<img src="https://source.unsplash.com/random/200x200?sig=5220">
+<img src="https://source.unsplash.com/random/200x200?sig=531">
+<img src="https://source.unsplash.com/random/200x200?sig=532">
+<img src="https://source.unsplash.com/random/200x200?sig=533">
+<img src="https://source.unsplash.com/random/200x200?sig=534">
+<img src="https://source.unsplash.com/random/200x200?sig=535">
+<img src="https://source.unsplash.com/random/200x200?sig=536">
+<img src="https://source.unsplash.com/random/200x200?sig=537">
+<img src="https://source.unsplash.com/random/200x200?sig=538">
+<img src="https://source.unsplash.com/random/200x200?sig=539">
+<img src="https://source.unsplash.com/random/200x200?sig=5310">
+<img src="https://source.unsplash.com/random/200x200?sig=5311">
+<img src="https://source.unsplash.com/random/200x200?sig=5312">
+<img src="https://source.unsplash.com/random/200x200?sig=5313">
+<img src="https://source.unsplash.com/random/200x200?sig=5314">
+<img src="https://source.unsplash.com/random/200x200?sig=5315">
+<img src="https://source.unsplash.com/random/200x200?sig=5316">
+<img src="https://source.unsplash.com/random/200x200?sig=5317">
+<img src="https://source.unsplash.com/random/200x200?sig=5318">
+<img src="https://source.unsplash.com/random/200x200?sig=5319">
+<img src="https://source.unsplash.com/random/200x200?sig=5320">
+<img src="https://source.unsplash.com/random/200x200?sig=5321">
+<img src="https://source.unsplash.com/random/200x200?sig=5322">
+<img src="https://source.unsplash.com/random/200x200?sig=5323">
+<img src="https://source.unsplash.com/random/200x200?sig=5324">
+<img src="https://source.unsplash.com/random/200x200?sig=5325">
+<img src="https://source.unsplash.com/random/200x200?sig=5326">
+<img src="https://source.unsplash.com/random/200x200?sig=5327">
+<img src="https://source.unsplash.com/random/200x200?sig=5328">
+<img src="https://source.unsplash.com/random/200x200?sig=5329">
+<img src="https://source.unsplash.com/random/200x200?sig=53210">
+<img src="https://source.unsplash.com/random/200x200?sig=53211">
+<img src="https://source.unsplash.com/random/200x200?sig=53312">
+<img src="https://source.unsplash.com/random/200x200?sig=53213">
+<img src="https://source.unsplash.com/random/200x200?sig=53214">
+<img src="https://source.unsplash.com/random/200x200?sig=53215">
+<img src="https://source.unsplash.com/random/200x200?sig=53216">
+<img src="https://source.unsplash.com/random/200x200?sig=53217">
+<img src="https://source.unsplash.com/random/200x200?sig=53218">
+<img src="https://source.unsplash.com/random/200x200?sig=53219">
+<img src="https://source.unsplash.com/random/200x200?sig=53220">
+</div>
+
+</body>
+
+<?php
+if(isset($_GET["beta"])){
+?>
+    <script src="js/lazy_simon_beta.js"></script>
+<?php
+}else{
+?>
+    <script src="js/lazy_simon.js"></script>
+<?php
+}
+?>
+</html>
---
+++ js/lazy_simon.js
@@ -0,0 +1,23 @@
+/*
+* lazy-simon.js
+*
+*  Minimal effort 350 byte JavaScript library to lazy load all <img> on your website
+* 
+* License: MIT (https://lazy.simon-frey.eu/mit.txt)
+*/
+o = new IntersectionObserver((a, s) => {
+  a.forEach(e => {
+    if (e.isIntersecting) {
+      e.target.src = e.target.dataset.l;
+      s.unobserve(e.target);
+    }
+  });
+});
+d = document.querySelectorAll("img");
+for (i = d.length - 1; i >= 0; i--) {
+  e = d[i];
+  e.dataset.l = e.src;
+  e.src =
+    "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mN89xQAAscB1RY/sEQAAAAASUVORK5CYII=";
+  o.observe(e);
+}
